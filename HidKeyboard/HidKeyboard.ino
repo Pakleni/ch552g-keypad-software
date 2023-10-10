62422710f974ecf6b960f53182685799499f8843
Ognjen Bjeletic <ognjenbjel@protonmail.com>
2023-10-10 14:41:34 +0200

    Format & brightness

--- HidKeyboard/HidKeyboard.ino
+++ HidKeyboard/HidKeyboard.ino
@@ -10,7 +10,6 @@
   modified by @pakleni
 
 */
-//键位定义
 #define KeyPad1 KEY_LEFT_ALT
 #define KeyPad2 'w'
 #define KeyPad3 'f'
@@ -60,44 +59,44 @@
 #define EC11A_PIN 31
 #define EC11B_PIN 30
 #define LEDPIN 34
-volatile int     mRotaryEncoderPulse        = 0;
+volatile int mRotaryEncoderPulse = 0;
 volatile uint8_t mLastestRotaryEncoderPinAB = 0; // last last pin value of A and B
-volatile uint8_t mLastRotaryEncoderPinAB    = 0; // last pin value of A and B
+volatile uint8_t mLastRotaryEncoderPinAB = 0;    // last pin value of A and B
 
 byte pins[] = {
-  BUTTON1_PIN,
-  BUTTON2_PIN,
-  BUTTON3_PIN,
-  BUTTON4_PIN,
-  BUTTON5_PIN,
-  BUTTON6_PIN,
+    BUTTON1_PIN,
+    BUTTON2_PIN,
+    BUTTON3_PIN,
+    BUTTON4_PIN,
+    BUTTON5_PIN,
+    BUTTON6_PIN,
 };
 
 byte keypad[] = {
-  KeyPad1,
-  KeyPad2,
-  KeyPad3,
-  KeyPad4,
-  KeyPad5,
-  KeyPad6,
+    KeyPad1,
+    KeyPad2,
+    KeyPad3,
+    KeyPad4,
+    KeyPad5,
+    KeyPad6,
 };
 
 bool pressPrev[] = {
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
 };
 
 byte delays[] = {
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
 };
 
 // This block sends 24 bits to the LED
@@ -114,9 +113,10 @@ byte delays[] = {
 // 300ns is 7.2 clock cycles
 // 600ns is 14.4 clock cycles
 // 900ns is 21.6 clock cycles
-void neopixel_show_long_P3_4(uint32_t dataAndLen) {
+void neopixel_show_long_P3_4(uint32_t dataAndLen)
+{
   //'dpl' (LSB),'dph','b' & 'acc'
-  //DPTR is the array address, B is the low byte of length
+  // DPTR is the array address, B is the low byte of length
   __asm__("    mov r3, b                           \n"
           ";save EA to R6                          \n"
           "    mov c,_EA                           \n"
@@ -147,89 +147,100 @@ void neopixel_show_long_P3_4(uint32_t dataAndLen) {
           "    mov a,r6                            \n"
           "    jz  skipRestoreEA_NP$               \n"
           "    setb  _EA                           \n"
-          "skipRestoreEA_NP$:                      \n"
-          );
-          (void)dataAndLen;
+          "skipRestoreEA_NP$:                      \n");
+  (void)dataAndLen;
 }
 
-// brightness is from 0 to 255
-#define BRIGHTNESS 60
+// brightness is from 0 to 1
+#define BRIGHTNESS 0.3
 #define COLOR_DELAY 1000
-byte RValue=0x00,GValue=0x00,BValue=BRIGHTNESS;
+byte RValue = 0x00, GValue = 0x00, BValue = 0xff;
 #define NUM_LEDS 6
-#define NUM_BYTES (NUM_LEDS*3)
+#define NUM_BYTES (NUM_LEDS * 3)
 __xdata uint8_t ledData[NUM_BYTES];
 
-#define set_pixel_for_GRB_LED(ADDR, INDEX, R, G, B)                            \
-  {                                                                            \
-    __xdata uint8_t *ptr = (ADDR) + ((INDEX)*3);                               \
-    ptr[0] = (G);                                                              \
-    ptr[1] = (R);                                                              \
-    ptr[2] = (B);                                                              \
+#define set_pixel_for_GRB_LED(ADDR, INDEX, R, G, B) \
+  {                                                 \
+    __xdata uint8_t *ptr = (ADDR) + ((INDEX)*3);    \
+    ptr[0] = (G);                                   \
+    ptr[1] = (R);                                   \
+    ptr[2] = (B);                                   \
   };
 
-#define neopixel_show_P3_4(ADDR, LEN)                                          \
-  neopixel_show_long_P3_4((((uint16_t)(ADDR)) & 0xFFFF) |                      \
+#define neopixel_show_P3_4(ADDR, LEN)                     \
+  neopixel_show_long_P3_4((((uint16_t)(ADDR)) & 0xFFFF) | \
                           (((uint32_t)(LEN)&0xFF) << 16));
 
-void handleColor() {
+void handleColor()
+{
   static byte colorDelay = 0;
   static byte mode = 0;
 
   // The color delay is used for delaying a color change
-  if (colorDelay > 0) {
+  if (colorDelay > 0)
+  {
     colorDelay--;
     return;
   }
 
-  switch(mode) {
-    case 0:
-      RValue++;
-      BValue--;
-      if (RValue == BRIGHTNESS) {
-        mode = 1;
-      }
-      break;
-    case 1:
-      GValue++;
-      RValue--;
-      if (GValue == BRIGHTNESS) {
-        mode = 2;
-      }
-      break;
-    case 2:
-      BValue++;
-      GValue--;
-      if (BValue == BRIGHTNESS) {
-        mode = 0;
-      }
-      break;
+  switch (mode)
+  {
+  case 0:
+    RValue++;
+    BValue--;
+    if (RValue == 0xff)
+    {
+      mode = 1;
+    }
+    break;
+  case 1:
+    GValue++;
+    RValue--;
+    if (GValue == 0xff)
+    {
+      mode = 2;
+    }
+    break;
+  case 2:
+    BValue++;
+    GValue--;
+    if (BValue == 0xff)
+    {
+      mode = 0;
+    }
+    break;
   }
 
   // Send color codes to the daisy chained LEDs
-  for(int i = 0; i < 6; i++) {
-    set_pixel_for_GRB_LED(ledData, i, RValue, GValue, BValue);
+  for (int i = 0; i < 6; i++)
+  {
+    set_pixel_for_GRB_LED(ledData, i, RValue * BRIGHTNESS, GValue * BRIGHTNESS, BValue * BRIGHTNESS);
   }
   neopixel_show_P3_4(ledData, NUM_BYTES);
-
 
   colorDelay = COLOR_DELAY;
 }
 
-void buttonPress(int i) {
+void buttonPress(int i)
+{
   bool press = !digitalRead(pins[i]);
-  if (pressPrev[i] != press) {
+  if (pressPrev[i] != press)
+  {
     pressPrev[i] = press;
-    if (press) {
+    if (press)
+    {
       Keyboard_press(keypad[i]);
-    } else {
+    }
+    else
+    {
       Keyboard_release(keypad[i]);
     }
-    delays[i]=50;
+    delays[i] = 50;
   }
 }
 
-void setup() {
+void setup()
+{
   USBInit();
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
@@ -241,14 +252,17 @@ void setup() {
   pinMode(EC11A_PIN, INPUT_PULLUP);
   pinMode(EC11B_PIN, INPUT_PULLUP);
 
-  pinMode(LEDPIN,OUTPUT);
-  digitalWrite(LEDPIN,LOW); 
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
 }
 
-void loop() {
+void loop()
+{
   // BUTTONS 1..6
-  for(int i = 0; i <6; i++) {
-    if (delays[i] > 0) {
+  for (int i = 0; i < 6; i++)
+  {
+    if (delays[i] > 0)
+    {
       delays[i]--;
       continue;
     }
@@ -259,46 +273,58 @@ void loop() {
   static bool pressPrev7 = false;
   static byte delay7 = 0;
 
-  if (delay7 > 0) {
+  if (delay7 > 0)
+  {
     delay7--;
   }
-  else {
+  else
+  {
     bool press = !digitalRead(EC11D_PIN);
-    if (pressPrev7 != press) {
+    if (pressPrev7 != press)
+    {
       pressPrev7 = press;
-      if (press) {
+      if (press)
+      {
         Consumer_press(KeyPad7);
-      } else {
+      }
+      else
+      {
         Consumer_release(KeyPad7);
       }
-      delay7=50;  //naive debouncing
+      delay7 = 50; // naive debouncing
     }
   }
 
   // BUTTON 8, 9
   static byte delay89 = 0;
-  if (delay89 > 0) {
+  if (delay89 > 0)
+  {
     delay89--;
   }
-  else {
+  else
+  {
     uint8_t currentPin = digitalRead(EC11A_PIN) * 10 + digitalRead(EC11B_PIN);
-    if (currentPin != mLastRotaryEncoderPinAB) {
-          if (mLastRotaryEncoderPinAB == 00) {
-            if (mLastestRotaryEncoderPinAB == 10 && currentPin == 01) {
-              Consumer_press(KeyPad8);
-              delay(10);
-              Consumer_release(KeyPad8);
-            }
-            else if (mLastestRotaryEncoderPinAB == 01 && currentPin == 10) {
-              Consumer_press(KeyPad9);
-              delay(10);
-              Consumer_release(KeyPad9);
-            }
-          }
-          mLastestRotaryEncoderPinAB = mLastRotaryEncoderPinAB;
-          mLastRotaryEncoderPinAB = currentPin;       
+    if (currentPin != mLastRotaryEncoderPinAB)
+    {
+      if (mLastRotaryEncoderPinAB == 00)
+      {
+        if (mLastestRotaryEncoderPinAB == 10 && currentPin == 01)
+        {
+          Consumer_press(KeyPad8);
+          delay(10);
+          Consumer_release(KeyPad8);
+        }
+        else if (mLastestRotaryEncoderPinAB == 01 && currentPin == 10)
+        {
+          Consumer_press(KeyPad9);
+          delay(10);
+          Consumer_release(KeyPad9);
+        }
+      }
+      mLastestRotaryEncoderPinAB = mLastRotaryEncoderPinAB;
+      mLastRotaryEncoderPinAB = currentPin;
     }
-    delay89=5;
+    delay89 = 5;
   }
   // Cycle the hue of the displayed color
   handleColor();
